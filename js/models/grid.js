48c2081d9a09cd8fed58ca09d64808f12cd1dd6b
Ricky K <rickyk.162@gmail.com>
2016-06-25 00:01:54 -0400

    New encode/decode method

--- js/models/grid.js
+++ js/models/grid.js
@@ -39,6 +39,12 @@ app.Grid = Backbone.Model.extend({
 		return this.get('grid')[r][c];
 	},
 
+	numberOfFilled: function() {
+		// Return the number of filled grid tiles
+
+		return _.flatten(this.get('grid')).reduce((x, y) => x + y);
+	},
+
 	equals: function(otherGrid) {
 		var thisFlat = _.flatten(this.get('grid'));
 		var otherFlat = _.flatten(otherGrid.get('grid'));
@@ -48,58 +54,58 @@ app.Grid = Backbone.Model.extend({
 	encode: function() {
 		// Encode the 2D grid into a 1D value
 
+		var numRowBlocks = this.getRows() / 5;
+		var numColBlocks = this.getCols() / 5;
+
 		var result = "";
-
-		for (let r = 0; r < this.getRows(); r++) {
-			result += this.compressRow(r) + ",";
+		for (let r = 0; r < numRowBlocks; r++) { // block row
+			for (let c = 0; c < numColBlocks; c++) { // block col
+				let curBlock = "";
+				for (let innerR = r * 5; innerR < r * 5 + 5; innerR++) // iterate thru rows in current block
+					curBlock += this.get('grid')[innerR].slice(c * 5, c * 5 + 5).join("");
+				result += this.compressBlock(curBlock);
+			}
+			result += ",";
 		}
-
 		return result.substr(0, result.length - 1);
 	},
 
 	decode: function(str) {
 		// Decode the string and set the model's size and grid to reflect
 
-		var rows = str.split(",");
-		var puzzleSize = new app.PuzzleSize({ rows: rows.length, cols: rows[0].length * 5 });
-
+		var blockRows = str.split(",");
+		var puzzleSize = new app.PuzzleSize({ rows: blockRows.length * 5, cols: blockRows[0].length });
 		if (puzzleSize.validate().success == false)
 			throw "Invalid size of puzzle. Rows/cols should be in multiples of 5.";
 
 		this.set({ 'size' : puzzleSize });
 		this.clearGrid();
 
-		for (let r = 0; r < rows.length; r++) {
-			// for each character in rows[r]
-				// convert character to decimal, and convert decimal to left padding binary
-				// iterate through binary representation of the columns to set the state
-			for (let i = 0; i < rows[r].length; i++) {
-				let char = rows[r].charAt(i);
-				let dec = this.decodeCharToNum(char);
-
-				if (dec < 0 || dec > 31)
-					throw "Decode out of range.";
-
-				let bin = app.utils.leftPad(app.utils.dec2bin(dec), "0", 5);
-				for (let c = 0; c < bin.length; c++) {
-					this.setState(r, (i * 5) + c, bin.charAt(c));
+		for (let blockR = 0; blockR < blockRows.length; blockR++) {
+			for (let blockC = 0; blockC < blockRows[0].length / 5; blockC++) { 
+				// for 5 charcters (representing the binary of 5 rows) starting from blockRows[blockR][blockC * 5]
+				for (let i = 0; i < 5; i++) {
+					// Decode each character and put setState appropriately
+					let dec = this.decodeCharToNum(blockRows[blockR][blockC * 5 + i]);
+					let bin = app.utils.leftPad(app.utils.dec2bin(dec), "0", 5);
+					for (let j = 0; j < bin.length; j++) {
+						this.setState(blockR * 5 + i, blockC * 5 + j, bin.charAt(j));
+					}
 				}
 			}
 		}
 	},
 
-	compressRow: function(rowIndex) {
-		var bin = this.get('grid')[rowIndex].join("");
-		var result = "";
-		
-		// Group columns in sets of 5 bits
-		for (let i = 0; i < bin.length; i += 5) {
-			let slice = bin.slice(i, i + 5);
+	compressBlock: function(bin) {
+		// Given a 5x5 block represented as a 25 character binary string, return the encoded version
+		if (bin.length != 25)
+			throw "Decode block size is invalid.";
 
-			// Convert the 5 char binary to decimal
-			let dec = parseInt(slice, 2);
+		var result = "";
+		for (let i = 0; i < 25; i += 5) {
+			let dec = parseInt(bin.substr(i, 5), 2);
 			result += this.encodeNumToChar(dec);
-		} 
+		}
 		return result;
 	},
 
